c165fdcd57b88e02a89458df2b10444fd9eb9dfc
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2019-07-13 20:19:45 +0530

    added teleop and sensor widgets

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # drones
 
 This repository contains all the code relevant to the drone exercises in the JdeRobot Robotics Academy.
+
+## Usage
+
+The packages provided here depend on the PX4 Firmware and SITL. We plan to provide this package as a deb file installable through `apt-get`, however, at the moment, the best method for the installation is to run the bash script to setup the dependancies and the catkin_ws and put the drone_wrapper package and rqt_follow_road package in the src.
--- rqt_follow_road/src/rqt_follow_road/follow_road.py
+++ rqt_follow_road/src/rqt_follow_road/follow_road.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import rospy
 import rospkg
@@ -6,12 +8,16 @@ from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 from python_qt_binding.QtGui import QIcon, QPixmap, QImage
+from python_qt_binding.QtCore import pyqtSignal, Qt
 
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 from std_msgs.msg import Bool, Float64
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose, PoseStamped, Twist, TwistStamped
+
+from teleopWidget import TeleopWidget
+from sensorsWidget import SensorsWidget
 
 class FollowRoad(Plugin):
 	def __init__(self, context):
@@ -24,8 +30,8 @@ class FollowRoad(Plugin):
 		parser = ArgumentParser()
 		# Add argument(s) to the parser.
 		parser.add_argument("-q", "--quiet", action="store_true",
-					  dest="quiet",
-					  help="Put plugin in silent mode")
+                      dest="quiet",
+                      help="Put plugin in silent mode")
 		# args, unknowns = parser.parse_known_args(context.argv())
 		# if not args.quiet:
 		# 	print 'arguments: ', args
@@ -48,10 +54,11 @@ class FollowRoad(Plugin):
 		if context.serial_number() > 1:
 			self._widget.setWindowTitle(
 				self._widget.windowTitle() + (' (%d)' % context.serial_number()))
-		
+
 		# Add logo
-		pixmap = QPixmap(os.path.join(rospkg.RosPack().get_path('rqt_follow_road'), 'resource', 'jderobot.png'))
-		self._widget.img_logo.setPixmap(pixmap.scaled(50,50))
+		pixmap = QPixmap(os.path.join(rospkg.RosPack().get_path(
+			'rqt_follow_road'), 'resource', 'jderobot.png'))
+		self._widget.img_logo.setPixmap(pixmap.scaled(50, 50))
 
 		# Set Variables
 		self.play_code_flag = False
@@ -63,17 +70,29 @@ class FollowRoad(Plugin):
 		self._widget.takeoffButton.clicked.connect(self.call_takeoff_land)
 		self._widget.playButton.clicked.connect(self.call_play)
 		self._widget.stopButton.clicked.connect(self.call_stop)
-		self._widget.resetButton.clicked.connect(self.call_reset)
 		self._widget.altdSlider.valueChanged.connect(self.alt_slider_val_changed)
 		self._widget.rotationDial.valueChanged.connect(self.rotation_val_changed)
 
 		# Add Publishers
 		self.takeoff_pub = rospy.Publisher('gui/takeoff_land', Bool, queue_size=1)
 		self.play_stop_pub = rospy.Publisher('gui/play_stop', Bool, queue_size=1)
-		self.alt_slider_pub = rospy.Publisher('gui/alt_slider', Float64, queue_size=1)
-		self.rotation_dial_pub = rospy.Publisher('gui/rotation_dial', Float64, queue_size=1)
+		self.twist_pub = rospy.Publisher('gui/twist', Twist, queue_size = 1)
+
+		#Add global variables
+		self.shared_twist_msg = Twist()
+		self.current_pose = Pose()
+		self.current_twist = Twist()
 
 		self.bridge = CvBridge()
+
+		self.teleop = TeleopWidget(self)
+		self._widget.tlLayout.addWidget(self.teleop)
+		self.teleop.setVisible(True)
+		self.sensors_widget = SensorsWidget(self)
+		self._widget.sensorsCheck.stateChanged.connect(self.show_sensors_widget)
+
+		# Add widget to the user interface
+		context.add_widget(self._widget)
 
 		# Add Subscibers
 		rospy.Subscriber('iris/cam_frontal/image_raw', Image, self.cam_frontal_cb)
@@ -81,19 +100,24 @@ class FollowRoad(Plugin):
 		rospy.Subscriber('interface/filtered_img', Image, self.filtered_img_cb)
 		rospy.Subscriber('interface/threshed_img', Image, self.threshed_img_cb)
 		rospy.Subscriber('mavros/local_position/pose', PoseStamped, self.pose_stamped_cb)
+		rospy.Subscriber('mavros/local_position/velocity_body', TwistStamped, self.twist_stamped_cb)
 
-		# Add widget to the user interface
-		context.add_widget(self._widget)
+	def show_sensors_widget(self, state):
+		if state == Qt.Checked:
+			self.sensors_widget.show()
+		else:
+			self.sensors_widget.hide()
 
 	def msg_to_pixmap(self, msg):
 		cv_img = self.bridge.imgmsg_to_cv2(msg)
 		h, w, _ = cv_img.shape
 		bytesPerLine = 3 * w
 		q_img = QImage(cv_img.data, w, h, bytesPerLine, QImage.Format_RGB888)
-		return QPixmap.fromImage(q_img).scaled(320,240)
+		return QPixmap.fromImage(q_img).scaled(320, 240)
 
 	def cam_frontal_cb(self, msg):
 		self._widget.img_frontal.setPixmap(self.msg_to_pixmap(msg))
+		self.sensors_widget.sensorsUpdate.emit()
 
 	def cam_ventral_cb(self, msg):
 		self._widget.img_ventral.setPixmap(self.msg_to_pixmap(msg))
@@ -103,10 +127,12 @@ class FollowRoad(Plugin):
 
 	def filtered_img_cb(self, msg):
 		self._widget.img_filtered.setPixmap(self.msg_to_pixmap(msg))
-	
+
 	def pose_stamped_cb(self, msg):
-		self._widget.XValue.setText('%.2f' % msg.pose.position.x)
-		self._widget.YValue.setText('%.2f' % msg.pose.position.y)
+		self.current_pose = msg.pose
+	
+	def twist_stamped_cb(self, msg):
+		self.current_twist = msg.twist
 
 	def call_takeoff_land(self):
 		if self.takeoff == True:
@@ -142,34 +168,42 @@ class FollowRoad(Plugin):
 			rospy.loginfo('Student code not running')
 			self._widget.term_out.append('Student code not running')
 
-	def call_reset(self):
-		rospy.loginfo('Resetting environment - Not yet implemented')
-		self._widget.term_out.append('Resetting environment - Not yet implemented')
-
 	def alt_slider_val_changed(self, value):
-		value = (1.0/(self._widget.altdSlider.maximum()/2)) * (value - (self._widget.altdSlider.maximum()/2))
+		value = (1.0/(self._widget.altdSlider.maximum()/2)) * \
+                    (value - (self._widget.altdSlider.maximum()/2))
 		self._widget.altdValue.setText('%.2f' % value)
 		rospy.logdebug('Altitude slider value changed to: %.2f', value)
-		self.alt_slider_pub.publish(Float64(value))
+		self.shared_twist_msg.linear.z = value
+		self.twist_pub.publish(self.shared_twist_msg)
 
 	def rotation_val_changed(self, value):
-		value = (1.0/(self._widget.rotationDial.maximum()/2)) * (value - (self._widget.rotationDial.maximum()/2))
+		value = (1.0/(self._widget.rotationDial.maximum()/2)) * \
+                    (value - (self._widget.rotationDial.maximum()/2))
 		self._widget.rotValue.setText('%.2f' % value)
 		rospy.logdebug('Rotational dial value changed to: %.2f', value)
-		self.rotation_dial_pub.publish(Float64(value))
+		self.shared_twist_msg.angular.z = value
+		self.twist_pub.publish(self.shared_twist_msg)
+	
+	def setXYValues(self, x, y):
+		self._widget.XValue.setText('%.2f' % -y)
+		self._widget.YValue.setText('%.2f' % -x)
+		rospy.logdebug('Teleop value changed to - x: %.2f y: %.2f', x, y)
+		self.shared_twist_msg.linear.x = -y
+		self.shared_twist_msg.linear.y = -x
+		self.twist_pub.publish(self.shared_twist_msg)
 
 	def shutdown_plugin(self):
-	# TODO unregister all publishers here
+		# TODO unregister all publishers here
 		pass
 
 	def save_settings(self, plugin_settings, instance_settings):
-	# TODO save intrinsic configuration, usually using:
-	# instance_settings.set_value(k, v)
+		# TODO save intrinsic configuration, usually using:
+		# instance_settings.set_value(k, v)
 		pass
 
 	def restore_settings(self, plugin_settings, instance_settings):
-	# TODO restore intrinsic configuration, usually using:
-	# v = instance_settings.value(k)
+		# TODO restore intrinsic configuration, usually using:
+		# v = instance_settings.value(k)
 		pass
 
 	#def trigger_configuration(self):
